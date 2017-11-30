9be1c002e109a7fe0169d7432949a90f8c2cafb8
philip fraser <pr.fraser3@gmail.com>
2017-11-30 10:59:05 +1000

    Initial express setup, added dog array

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
-console.log('Server running!!')
+const dogs = ['Doggo', 'Pupper', 'Woofer']
+
+app.get('/api/dogs', (req, res) => {
+	// Send the dogs array to the user
+	res.send(dogs)
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+console.log('Server running!!');
