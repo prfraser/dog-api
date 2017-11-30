cf12c0da494d1a101b02011e1f85a8528ecb0f19
philip fraser <pr.fraser3@gmail.com>
2017-11-30 11:45:40 +1000

    fixed cors error on front-end, commented out express fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 
 const dogs = ['Doggo', 'Pupper', 'Woofer']
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/api/dogs', (req, res) => {
 	// Send the dogs array to the user
